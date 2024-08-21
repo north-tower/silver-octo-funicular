6b94543009acbb0fb13c65437c0840d18226df43
Miki <muneshmyke@gmail.com>
2024-08-21 17:29:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const path = require("path")
 
 const corsOptions = {
   // origin: 'https://dom-shop-frontend.vercel.app',
-  origin: 'https://fir-upload-a9558.web.app/',
+  origin: 'https://fir-upload-a9558.web.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
 };
