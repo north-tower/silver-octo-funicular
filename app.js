140d78706b31e5a711b42370343e5ab015606449
Miki <muneshmyke@gmail.com>
2024-08-27 15:09:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const path = require("path")
 
 const corsOptions = {
   // origin: 'https://dom-shop-frontend.vercel.app',
-  origin: 'http://localhost:3000',
+  origin: 'https://silver-octo-funicular-delta.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
 };
